1cf7d27285b3677bbf945f3df306735cb22e3d77
Peter <peter@insella.se>
2013-10-21 15:55:34 +0200

    started on a simple editor interface

--- src/main/scala/se/stagehand/App.scala
+++ src/main/scala/se/stagehand/App.scala
@@ -3,22 +3,37 @@ package se.stagehand
 import scala.swing._
 import se.stagehand.plugins._
 import java.io.File
+import scala.swing.BorderPanel.Position._
 
 /**
  * @author ${user.name}
  */
-object App {
-    
-  def main(args : Array[String]) {
-    val frame = new Frame
-    val panel = new BorderPanel
-    
-    PluginManager.init
-    
-    frame.contents = panel
-    
-    frame.visible = true
-    
+object App extends SimpleSwingApplication {
+  
+  val plugins = PluginManager.getPlugins
+  
+  def top = new MainFrame {
+    title = "Stagehand Editor"
+    contents = ui
   }
+  
+  def ui = new BorderPanel {
+   
+    layout(sidemenu) = West
+  }
+  
+  def sidemenu = {
+    val pan = new FlowPanel() {
+      border = Swing.EmptyBorder(5,5,5,5)
+    }
+    plugins.foreach( pi =>
+      pi.getEffects.foreach(ef => 
+      	pan.contents += ef
+      )
+    )
+    
+    pan
+  }
+  
 
 }
