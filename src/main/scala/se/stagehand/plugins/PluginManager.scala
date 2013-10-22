50cf8213c1b4b22416ebf756372326461f77fb4d
Peter Holm <peter@insella.se>
2013-10-22 15:57:22 +0200

    expanded to handle scripts plugins as well

--- src/main/scala/se/stagehand/plugins/PluginManager.scala
+++ src/main/scala/se/stagehand/plugins/PluginManager.scala
@@ -10,9 +10,13 @@ import scala.xml.XML
 
 object PluginManager {
   
-  private var pluginMap = Map[String, EffectPlugin]()
+  private var effectsMap = Map[String, EffectPlugin]()
+  private var scriptMap = Map[String, ScriptPlugin]()
   
-  def init() {
+  /**
+   * Load all plugins into the program.
+   */
+  private def init() {
     val pluginDir = new File("plugins")
     val files = pluginDir.listFiles(new FileFilter(){
       def accept(x:File) = x.getPath().endsWith("jar")
@@ -20,24 +24,43 @@ object PluginManager {
     
     val classes = files.map(loadPluginClass(_))
     
-    classes.foreach(plugin => pluginMap += (plugin.name -> plugin))
+    classes.foreach(_ match {
+      case p if p.isInstanceOf[EffectPlugin] => effectsMap += (p.name -> p.asInstanceOf[EffectPlugin])
+      case p if p.isInstanceOf[ScriptPlugin] => scriptMap += (p.name -> p.asInstanceOf[ScriptPlugin])
+    })
     
   }
   
-  def getPlugin(name: String):Option[EffectPlugin] = {
-    if (pluginMap.isEmpty) init
-    pluginMap.get(name)
+  /**
+   * Fetch an effect plugin by name. 
+   */
+  def effectPlugin(name: String):Option[EffectPlugin] = {
+    if (effectsMap.isEmpty) init
+    effectsMap.get(name)
   }
   
-  def getPlugins():Array[EffectPlugin] = {
-    if (pluginMap.isEmpty) init
-    pluginMap.toArray.map(_._2)
+  def effectPlugins():Array[EffectPlugin] = {
+    if (effectsMap.isEmpty) init
+    effectsMap.toArray.map(_._2)
+  }
+  
+  /**
+   * Find a script plugin by name. 
+   */
+  def scriptPlugin(name: String):Option[ScriptPlugin] = {
+    if (scriptMap.isEmpty) init
+    scriptMap.get(name)
+  }
+  
+  def scriptPlugins():Array[ScriptPlugin] = {
+    if (scriptMap.isEmpty) init
+    scriptMap.toArray.map(_._2)
   }
   
   /**
    * Clumsy way to load classes
    */
-  private def loadPluginClass(jar: File):EffectPlugin = {
+  private def loadPluginClass(jar: File):Plugin = {
     val file = new JarFile(jar)
     val e = file.entries()
     val urls = Array( jar.toURL() )
@@ -57,6 +80,7 @@ object PluginManager {
     }
     val stream = file.getInputStream(conf)
     val xml = XML.load(stream)
+    val supertype = (xml \\ "supertype").text
     val classname = (xml \\ "pluginclass").text
     
     classes = classes.filter(_.endsWith(classname))
@@ -64,6 +88,6 @@ object PluginManager {
     
     val c = urlLoader.loadClass(classes.head)
     
-    c.newInstance().asInstanceOf[EffectPlugin]
+    c.newInstance().asInstanceOf[Plugin]
   }
 }
