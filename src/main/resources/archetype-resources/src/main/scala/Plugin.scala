c7271daa13cd91ab8591c1557ef59f489ea340e6
Peter <peter@insella.se>
2013-10-23 16:47:48 +0200

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/scala/Plugin.scala
@@ -0,0 +1,12 @@
+package ${groupId}
+
+import se.stagehand.plugins.EffectPlugin
+import se.stagehand.lib.scripting.Effect
+
+class ${artifactId}Plugin extends EffectPlugin {
+
+  val name = "${artifactId}"
+    
+  def getEffects(): Array[Effect] = Array(new ${artifactId}Effect)
+  
+}
