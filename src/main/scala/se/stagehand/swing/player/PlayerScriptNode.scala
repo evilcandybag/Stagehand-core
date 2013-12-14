4d460c70d55bb941bddaf74466da07344d7e1314
Peter Holm <peter@insella.se>
2013-12-14 02:39:08 +0100

    Some extra functionality for having nodes show up in Player

--- src/main/scala/se/stagehand/swing/player/PlayerScriptNode.scala
+++ src/main/scala/se/stagehand/swing/player/PlayerScriptNode.scala
@@ -3,9 +3,20 @@ package se.stagehand.swing.player
 import se.stagehand.lib.scripting.ScriptComponent
 import scala.swing.BorderPanel
 import se.stagehand.swing.lib.ScriptNode
+import scala.swing._
+import java.awt.Color
 
 abstract class PlayerScriptNode[T <: ScriptComponent](sc:T) extends BorderPanel with ScriptNode[T] {
   private val _script: T = sc
   def script = _script
+  
+  border = Swing.CompoundBorder(Swing.EtchedBorder(Swing.Raised),Swing.EtchedBorder(Swing.Raised))
 
+  def title:String
+  
+  layout(new Label(title)) = BorderPanel.Position.North
+  
+  revalidate
+  repaint
+  peer.setSize(preferredSize)
 }
