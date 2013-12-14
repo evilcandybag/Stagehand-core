d5c1fae920175e03dca2fdb2d43f259ae4b9dd8d
Peter Holm <peter@insella.se>
2013-12-14 02:20:01 +0100

    Prototypes now store the componentName related to the corresponding class

--- src/main/scala/se/stagehand/lib/scripting/ID.scala
+++ src/main/scala/se/stagehand/lib/scripting/ID.scala
@@ -9,7 +9,7 @@ object ID {
   
   private var counter:Int = 0
   private var pool: Map[Int,StagehandComponent] = Map()
-  private var prototypes: Set[Class[_]] = ListSet()
+  private var prototypes: Map[Class[_],String] = Map()
   
   def unique:Int = {
     counter += 1
@@ -34,14 +34,14 @@ object ID {
     //in practice, the first component that is created is the prototype
     val p = prototypes.contains(sc.getClass)
     if (!p) {
-      prototypes += sc.getClass
+      prototypes += (sc.getClass -> sc.componentName)
     }
     pool += (i -> sc)
   }
   def newInstance[T <: StagehandComponent](name:String):T = {
-    val p = prototypes.find(x => x.getName == name)
+    val p = prototypes.find(x => x._1.getName == name)
     if (p.isDefined) {
-      return p.get.newInstance().asInstanceOf[T]
+      return p.get._1.newInstance().asInstanceOf[T]
     } else {
       throw new IllegalArgumentException("class not found")
     }
@@ -54,6 +54,18 @@ object ID {
   def newInstance[T <: StagehandComponent](sc: T):T = {
     newInstance[T](sc.getClass)
   }
+  
+  /**
+   * Get all subclasses of this class, from all StagehandComponents loaded into the program.
+   * They come in pairs with their componentName.
+   */
+  def getAll(c: Class[_]):Seq[Class[_]] = {
+    getAllWithNames(c).map(_._1)
+  }
+  def getAllWithNames(c: Class[_]):Seq[(Class[_],String)] = {
+    prototypes.filter(x => c.isAssignableFrom(x._1)).toSeq
+  }
+  def getName(c: Class[_]):Option[String] = prototypes.get(c)
   
   def fetch[T <: StagehandComponent](i:Int): T = {
     val sc = pool.apply(i)
