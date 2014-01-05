83ff95e96398bb39e5cf46d019a344469b48964c
Peter Holm <peter@insella.se>
2014-01-05 15:46:55 +0100

    Player nodes are now clickable

--- src/main/scala/se/stagehand/swing/player/PlayerScriptNode.scala
+++ src/main/scala/se/stagehand/swing/player/PlayerScriptNode.scala
@@ -5,6 +5,9 @@ import scala.swing.BorderPanel
 import se.stagehand.swing.lib.ScriptNode
 import scala.swing._
 import java.awt.Color
+import scala.swing.event.MouseClicked
+import java.awt.event.MouseEvent.{BUTTON1}
+
 
 abstract class PlayerScriptNode[T <: ScriptComponent](sc:T) extends BorderPanel with ScriptNode[T] {
   private val _script: T = sc
@@ -15,6 +18,12 @@ abstract class PlayerScriptNode[T <: ScriptComponent](sc:T) extends BorderPanel 
   def title:String
   
   layout(new Label(title)) = BorderPanel.Position.North
+  listenTo(mouse.clicks)
+  reactions += {
+    case e:MouseClicked if e.peer.getButton == BUTTON1 => {
+      script.executeInstructions()
+    }
+  }
   
   revalidate
   repaint
