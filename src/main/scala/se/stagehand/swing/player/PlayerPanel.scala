9a86f86f513522e2d74ec2debe06edb2c0f92771
Peter Holm <peter@insella.se>
2014-01-05 15:46:17 +0100

    Better loading

--- src/main/scala/se/stagehand/swing/player/PlayerPanel.scala
+++ src/main/scala/se/stagehand/swing/player/PlayerPanel.scala
@@ -12,20 +12,46 @@ import se.stagehand.swing.lib.ScriptGUI
 import se.stagehand.swing.lib.OutputGUI
 import scala.xml._
 import java.awt.Color
+import se.stagehand.lib.scripting.Effect
+import se.stagehand.lib.scripting.StagehandComponent
+import scala.swing.event.KeyEvent
+import scala.swing.event.Event
+import se.stagehand.swing.lib.KeyEventHandler
+import se.stagehand.lib.Log
 
 class PlayerPanel extends NullPanel {
+  private val log = Log.getLog(this.getClass())
+  
   preferredSize = new Dimension(500,500)
   border = Swing.EtchedBorder(Swing.Raised)
+  listenTo(keys)
+  deafTo(this)
+  focusable = true
+  requestFocus
+  
+  
+  reactions += {
+    case e:Event => {log.debug(e.toString); publish(e)}
+  }
   
   def fromXML(xml:Node) {
+    ID.clearInstances
     val scxml = (xml \\ "components") \ "script"
+    val fxxml = (xml \\ "components") \ "effect"
     val scripts = for (n <- scxml) yield {
-      ScriptComponent.fromXML[ScriptComponent](n)
+      StagehandComponent.fromXML[ScriptComponent](n)
     }
+    val effects = for (n <- fxxml) yield {
+      StagehandComponent.fromXML[Effect](n)
+    }
+    (scxml ++ fxxml).foreach(n => {
+      val id = (n \ "id").text.toInt
+      
+      ID.fetch[StagehandComponent](id).readInstructions(n)
+    })
     
     
     val nxml = (xml \\ "nodes") \ "node"
-    println("nxml: " + nxml)
     val nodes = for (n <- nxml if n.label == "node") yield {
       val id = (n \ "id").text.toInt
       val pos = new Point((n \ "locx").text.toInt, (n \ "locy").text.toInt)
@@ -37,7 +63,6 @@ class PlayerPanel extends NullPanel {
       val pn = gui.playerNode(sc)
       GUIManager.register(pn)
       add(pn,pos)
-      println("added " + pn.getClass())
       
       sc
     } 
