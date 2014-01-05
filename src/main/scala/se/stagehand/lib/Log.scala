201463ddce51b71a76a5d4b537f050dcf6bba95c
Peter Holm <peter@insella.se>
2014-01-05 15:39:49 +0100

    Added support for logging system

---
+++ src/main/scala/se/stagehand/lib/Log.scala
@@ -0,0 +1,73 @@
+package se.stagehand.lib
+
+import java.io.OutputStream
+import java.io.PrintStream
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+object Log {
+  object Mode extends Enumeration {
+    val NoLog,NoDebug,Default = Value
+  }
+  
+  private var _logs: Map[Class[_],Log] = Map()
+  private var _out: PrintStream = System.out
+  var mode = Mode.Default
+  var showTimestamp = false
+  private var _dateFormat = new SimpleDateFormat("HH:mm:ss")
+  
+  
+  private def activeLog(cls:Class[_]) = {
+    mode match {
+      case Mode.Default => new Log(cls)
+      case Mode.NoLog => NoLog
+      case Mode.NoDebug => new NoDebug(cls)
+    }
+  }
+  def timestamp = {
+    _dateFormat.format(Calendar.getInstance.getTime)
+  }
+    
+  def out_=(stream:PrintStream) = _out = stream
+  def dateFormat_=(str:String) = {
+    _dateFormat.applyPattern(str)
+  }
+  
+  def getLog(cls:Class[_]): Log = {
+    _logs.get(cls) match {
+      case Some(l) => l
+      case None    => {
+        val log = activeLog(cls)
+        _logs += (cls -> log)
+        log
+      }
+    }
+  }
+  
+  def out = _out
+  
+  private object NoLog extends Log(classOf[Any]) {
+    override def debug(msg:String) {}
+    override def log(msg:String) {}
+  }
+  private class NoDebug(cls:Class[_]) extends Log(cls) {
+    override def debug(msg:String) {}
+  }
+  
+}
+
+class Log(private val cls:Class[_]) {
+  private val cname = {
+    val nm = cls.getName
+    val i = nm.lastIndexOf('.') + 1
+    
+    nm.substring(i, nm.length)
+  }
+  
+  private def stamp = if (Log.showTimestamp) Log.timestamp + "-" else ""
+  
+  def debug(msg:String) = message("DEBUG", msg)
+  def log(msg:String) = message("LOG", msg)
+  
+  private def message(typ:String, msg:String) = Log.out.println("[" + stamp + typ + "-" + cname + "] " + msg)
+}
--- src/main/scala/se/stagehand/lib/scripting/Output.scala
+++ src/main/scala/se/stagehand/lib/scripting/Output.scala
@@ -2,11 +2,13 @@ package se.stagehand.lib.scripting
 
 import scala.collection.immutable.ListSet
 import scala.xml.Elem
+import se.stagehand.lib.Log
 
 /**
  * The implementing ScriptComponent can produce an output signal.
  */
 trait Output extends ScriptComponent {
+  private val log = Log.getLog(this.getClass())
   
   private var _receivers: Set[ScriptComponent with Input] = ListSet()
   
@@ -14,6 +16,7 @@ trait Output extends ScriptComponent {
    * connect a receiver to this output
    */
   def connectOut(rec: ScriptComponent with Input) {
+    log.debug("Connected " + rec)
     _receivers = _receivers + rec
   }
   
@@ -33,9 +36,10 @@ trait Output extends ScriptComponent {
    * Signal all connected Inputs with the given ControlMessage.
    */
   def signal(msg: ScriptMessage) {
-    receivers.foreach(_ ! msg)
+    log.debug(receivers.toString)
+    receivers.foreach(x => {log.debug("messaging: " + x.displayName); x ! msg})
   }
   
   def outputsXML: Elem = 
-    <outputs>{_receivers.map(_.idXML)}</outputs>
+    <outputs>{log.debug(_receivers.toString);_receivers.map(_.idXML)}</outputs>
 }
