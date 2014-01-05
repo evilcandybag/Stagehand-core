9beb7bc42f5046633711b5d6f8c9d4d98d7e1254
Peter Holm <peter@insella.se>
2014-01-05 15:40:16 +0100

    more work on AST implementation

--- src/main/scala/se/stagehand/lib/scripting/ast/Stage.scala
+++ src/main/scala/se/stagehand/lib/scripting/ast/Stage.scala
@@ -3,26 +3,32 @@ package se.stagehand.lib.scripting.ast
 import java.awt.Point
 import scala.xml._
 
-final case class Stage
-
 sealed abstract class ASTNode {
-  def toXML: Node
-  protected def typ: String
-  
+  def typ: String
 }
 
-sealed abstract class Component(c: Class[_], i:Int) {
-  val cls:Class[_] = c; val id:Int = i
-  
-//  def toXML = 
-//    <component class={cls.getName}
-//             id={i.toString}></component>.copy(label = typ)
-    
-}
-abstract class Script(c: Class[_], id:Int) extends Component(c,id)
-abstract class Effect(c: Class[_], id:Int) extends Component(c,id)
+sealed case class Stage(components: List[Component], ui: List[UINode])
 
-trait Outputs extends Script
+sealed abstract class Component(cls: String, i:Int, p: List[Prop]) extends ASTNode {
+
+}
+case class Script(c: String, id:Int, p:List[Prop]) extends Component(c,id,p) {
+  def typ = "script"
+}
+case class Effect(c: String, id:Int, p:List[Prop]) extends Component(c,id,p) {
+  def typ = "effect"
+}
+
+sealed trait Prop
+case class Val(name: String, value: String) extends Prop
+case class PList(name: String, vals: List[Val]) extends Prop
 
 //Represents a node in an UI
 final case class UINode(id: Int, pos: Point)
+
+object examples {
+  val tree = {
+    val e = Effect("Popup",1,List(Val("message","bajs")))
+    
+  }
+}
