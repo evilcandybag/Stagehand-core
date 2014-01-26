06747ab7fd9107e8f4b8b6f116d1ef32f08c262b
Peter Holm <peter@insella.se>
2014-01-26 12:56:25 +0100

    Added image asset for default background image

--- Stagehand-swing/src/main/scala/se/stagehand/swing/assets/ImageAssets.scala
+++ Stagehand-swing/src/main/scala/se/stagehand/swing/assets/ImageAssets.scala
@@ -3,10 +3,20 @@ package se.stagehand.swing.assets
 import javax.swing.ImageIcon
 import se.stagehand.lib.Log
 import scala.swing.Dialog
+import java.io.File
 
 object ImageAssets {
   private val log = Log.getLog(this.getClass())
   
-  val TARGET_ICON = new ImageIcon("assets/target.png")
   
+  /**
+   * File references
+   */
+  val TARGET = new File("assets/target.png")
+  val BACKGROUND_PLACEHOLDER = new File("assets/bg-placeholder.png")
+  
+  /**
+   * Image icon references
+   */
+  val TARGET_ICON = new ImageIcon(TARGET.getPath())
 }
