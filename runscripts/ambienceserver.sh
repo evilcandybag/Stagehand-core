8165807bb71c781aa4a10732c88d74a36422bce9
Peter <peter@insella.se>
2014-02-20 15:20:44 +0100

    Fixed erroneous path

--- runscripts/ambienceserver.sh
+++ runscripts/ambienceserver.sh
@@ -1,1 +1,1 @@
-java -cp ../stagehand.jar:../plugins/Stagehand-Ambience.jar se.stagehand.plugins.ambience.server.AmbienceServer
+java -cp ../stagehand.jar:../plugins/Stagehand-Ambience.jar se.stagehand.plugins.ambience.server.AmbienceServer $1
